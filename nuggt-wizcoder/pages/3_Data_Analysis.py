6feb0f717c46e9f0aba5383440d46c924667b193
Shoib Loya <52075633+shoibloya@users.noreply.github.com>
2023-07-04 19:41:04 +0800

    Update Changed Text Prompt for Data Analysis Page

--- nuggt-wizcoder/pages/3_Data_Analysis.py
+++ nuggt-wizcoder/pages/3_Data_Analysis.py
@@ -7,10 +7,8 @@ user_input = """Print the head of {upload:data} with print() using {tool:python}
 output_format = "I have displayed the visualisations using streamlit"
 
 st.subheader("Data Analysis")
-st.markdown("""Upload an Excel/CSV file for data analysis. You can ask the app generic questions like "summarise this data" or specific questions like "What is the relationship between Column A and Column B". The app can also generate data visualisations like bargraphs, piecharts etc. You can generate visualisations by typing "Generate visualisations for..". This app was created with a single AI command,             
-""")
-st.markdown("""Open **:red[{ upload : file }]** using **:green[{ tool : python }]**. Display its description using **:green[{ tool : python }]** using **:green[{ tool : python }]**. Complete the task **:blue[{ text : input }]** using **:green[{ tool : python }]**. Display your results using **:green[{ tool : data_analysis }]**.  \n
-""")
+st.markdown("""The following prompt was used to create this Nuggt App:""")
+st.markdown("""Print the head of {upload:data} with print() using {tool:python}. Create a bar plot between {text:column1} column and {text:column2} column with matplotlib using {tool:python}. Display the bar plot with proper labels with streamlit library using {tool:python}\n""")
 
 if user_input and output_format:
     variables = extract_variables(user_input)
