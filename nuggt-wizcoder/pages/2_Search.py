7cd981e20d8e50c6a7bed53cb433bc26c8b08395
Shoib Loya <52075633+shoibloya@users.noreply.github.com>
2023-07-04 19:44:47 +0800

    Update Search Page Text

--- nuggt-wizcoder/pages/2_Search.py
+++ nuggt-wizcoder/pages/2_Search.py
@@ -10,8 +10,7 @@ output_format = "I have displayed the results in the given format."
 
 st.subheader("Search")
 st.markdown("**Ask a query and Nuggt would provide you answers along with the relevant sources. This app was created with a single AI command,**")
-st.markdown("""Find websites related to **:blue[{ text : query }]** using **:green[{ tool : google }]**. Browse three results to gather information on **:blue[{ text : query }]** using **:green[{ tool : browse_website }]**. Display the results in the format **<Content: Content of the website, URL: URL of the website>** using **:green[{ tool : display }]**  \n
-""")
+st.markdown("**Do take not that the search tool has not been tested with the Wizcoder Version. However, feel free to explore.**")
         
 if user_input and output_format:
     variables = extract_variables(user_input)
